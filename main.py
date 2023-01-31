27a41687b6bc312c27fbad36b17980544046c6c6
Kontik <oksioksana555@gmail.com>
2023-01-31 12:06:12 +0200

    23

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from pynput import keyboard
 
-hotkey = keyboard.HotKey(
-    keyboard.HotKey.parse('<ctrl>+<alt>+h'),
-    on_activate)
+cmb = [{keyboard.Key.caps_lock}]
+
+current = set()
+def execute():
+    print("Detected hotkey")
+
+def on_press(key):
+    if any([key in z for z in cmb]):
+        current.add(key)
+        if any(all(k in current for k in z) for z in cmb):
+            execute()
+
+def on_release(key):
+    if any([key in z for z in cmb]):
+        current.remove(key)
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
