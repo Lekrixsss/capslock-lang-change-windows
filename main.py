af15e11fddbae35214be504541b1e27b711e66ef
Kontik <atix.atix6@gmail.com>
2023-02-09 16:12:29 +0200

    Edited

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-from pynput import keyboard
-
-cmb = [{keyboard.Key.caps_lock}]
-
-current = set()
-def execute():
-    print("Detected hotkey")
-
-def on_press(key):
-    if any([key in z for z in cmb]):
-        current.add(key)
-        if any(all(k in current for k in z) for z in cmb):
-            execute()
-
-def on_release(key):
-    if any([key in z for z in cmb]):
-        current.remove(key)
-
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+import keyboard
+keyboard.remap_hotkey('caps_lock', 'shift+alt')
+keyboard.remap_hotkey('shift+caps_lock', 'caps_lock')
+keyboard.wait()
